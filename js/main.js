c0785a2e417944a3520d0bbcb96987fff317c268
UcYusuke <hayanobori61@yahoo.co.jp>
2023-04-09 10:10:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -120,6 +120,14 @@
 
 
 
+    //スライダー
+    $(".slide-items").slick({
+        autoplay:true,
+        slidesToShow:3,
+        infinite:true,
+        slidesToScroll:1,
+        dots:true,
+      });
 
 
 }
